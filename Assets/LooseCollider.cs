6207e28dbbf684bb79c92c235be2bb243ddf32c5
Fernando <fernando@aguilar.net.br>
2017-04-15 08:14:17 -0300

    Making loose menu, menu animation, game scene

---
+++ Assets/LooseCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCollider : MonoBehaviour {
+	private LevelMananger mananger;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		print("Perdeu");
+		mananger = GameObject.FindObjectOfType<LevelMananger>();
+		mananger.LoadLevel("Loose");
+	}
+}
---
+++ Assets/Shredder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		print("Triggered, destroying");
+		Destroy(col.gameObject);
+	}
+}
