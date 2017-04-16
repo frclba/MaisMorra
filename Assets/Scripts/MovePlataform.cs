b408d0fef100b63197b223c5545f2e3ee6c30479
Fernando <fernando@aguilar.net.br>
2017-04-16 08:37:53 -0300

    Making level Line prefabs, trail renderer and Line Dificulties

--- Assets/Scripts/MovePlataform.cs
+++ Assets/Scripts/MovePlataform.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovePlataform : MonoBehaviour {
 
-	public float platformSpeed = 5f;
+	public float platformSpeed = 0f;
 	public float maximum_posY = 7f;
 
 	// Use this for initialization
