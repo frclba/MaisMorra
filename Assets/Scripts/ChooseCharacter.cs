d468a42580feba0a12fb0a99f612f87550a7fa67
Fernando <fernando@aguilar.net.br>
2017-04-16 02:58:53 -0300

    Chooser Controller & player selection

---
+++ Assets/Scripts/ChooseCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCharacter : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject instanciado;
+
+	public int id = 0;
+	public string select = "jump";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Rodou");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown(select)){
+			instanciado = Instantiate(player, new Vector2(0,0), Quaternion.identity) as GameObject;
+			player.GetComponent<ChooserController>().id = id;
+			id++;
+		}
+	}
+}
---
+++ Assets/Scripts/ChooserController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooserController : MonoBehaviour {
+
+	public int id;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(id);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
